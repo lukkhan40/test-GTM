2685f1e8bc9870bfe9a077fb3740a1f1762b8714
lukkhan40 <lukkhan40@gmail.com>
2023-06-16 10:54:28 -0600

    remove files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-//import "./index.css";
+import "./index.css";
 import { Root } from "./routes/root";
 import { ErrorPage } from "./routes/error";
 import { Event } from "./routes/event";
 import { Purchase } from "./routes/purchase";
-import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter([
   {
@@ -30,8 +29,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
