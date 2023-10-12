f7b567f307ad594f1d309d07037d4a756ed3c745
lukkhan40 <lukkhan40@gmail.com>
2023-10-12 10:24:48 -0600

    add test inputs

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 
 export const Event = () => {
+  const [inputValue, setInputValue] = useState("")
+
+  const onChange = (e) => {
+    const { value } = e.target
+    setInputValue(value)
+  }
+
+  const okButton = () => {
+    console.log(inputValue);
+  }
+
   return (
-    <div>
+    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", border: "1px solid black" }}>
       <Helmet>
         <title>Event</title>
         <meta property="og:title" content="Event" />
@@ -14,7 +25,15 @@ export const Event = () => {
         <meta name="description" content="Descripcion del evento" />
         <meta property="og:description" content="Descripcion del evento" />
       </Helmet>
-      Page Event
+      <form>
+        <input />
+        <button>OK form button</button>
+      </form>
+      <div style={{ marginTop: "10px" }}>
+        <input value={inputValue} onChange={onChange} />
+        <button>OK button</button>
+      </div>
+      <div style={{ marginTop: "10px" }}><input placeholder="simpleInput" /></div>
     </div>
   );
 };
